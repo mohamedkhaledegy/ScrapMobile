852a7c7dd575bc88a843ffa364b213bccceacd94
mohamedkhaledegy <74407534+mohamedkhaledegy@users.noreply.github.com>
2021-11-12 07:29:36 +0200

    edit url

--- mobile/urls.py
+++ mobile/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index , name='index' ),
-    path('sell/',views.sell , name='selll' ),
+    path('sell/',views.sell , name='sell' ),
     path('<slug:slug>/' ,views.mobile , name='mobile'), # صفحة الموبايل
     path('brand/<slug:slug>/' ,views.brand_mobs , name='brand_mobiles_list')
     ]
